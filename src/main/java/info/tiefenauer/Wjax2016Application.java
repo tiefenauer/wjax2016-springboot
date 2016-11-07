9dae7713f0df0caf0e84bc3a9935efa4a8cb59e0
Daniel Tiefenauer <git@tiefenauer.info>
2016-11-07 09:52:26 +0100

    initial commit

---
+++ src/main/java/info/tiefenauer/Wjax2016Application.java
@@ -0,0 +1,12 @@
+package info.tiefenauer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wjax2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wjax2016Application.class, args);
+	}
+}
---
+++ src/test/java/info/tiefenauer/Wjax2016ApplicationTests.java
@@ -0,0 +1,16 @@
+package info.tiefenauer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Wjax2016ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
