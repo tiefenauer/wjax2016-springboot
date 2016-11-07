7d397cabe0b343146ad654a39da0ee807121d2e0
Daniel Tiefenauer <git@tiefenauer.info>
2016-11-07 16:44:20 +0100

    add traefik config

--- src/main/java/info/tiefenauer/Wjax2016Application.java
+++ src/main/java/info/tiefenauer/Wjax2016Application.java
@@ -1,11 +1,16 @@
 package info.tiefenauer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.TwitterAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = {TwitterAutoConfiguration.class})
 public class Wjax2016Application {
 
 	public static void main(String[] args) {
