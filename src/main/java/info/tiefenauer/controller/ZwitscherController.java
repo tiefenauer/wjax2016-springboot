54047f0e1ef60595a5fa0b06d389bafa571fa739
Daniel Tiefenauer <git@tiefenauer.info>
2016-11-07 11:19:51 +0100

    add custom query and limit

--- src/main/java/info/tiefenauer/controller/ZwitscherController.java
+++ src/main/java/info/tiefenauer/controller/ZwitscherController.java
@@ -6,10 +6,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -27,9 +26,11 @@ public class ZwitscherController {
         this.repository = repository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public HttpEntity<Collection<String>> tweets(){
-        Collection<String> tweets = repository.search("cloudnativenerd", 23);
+    @RequestMapping(path = "/{q}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public HttpEntity<Collection<String>> tweets(
+            @PathVariable("q") String q,
+            @RequestParam(name = "limit", defaultValue = "10") int limit){
+        Collection<String> tweets = repository.search(q, limit);
         return new ResponseEntity<>(tweets, HttpStatus.OK);
     }
 }
