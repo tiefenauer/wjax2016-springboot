204a90874f197e957477ea445cdafd8fec7ce454
Daniel Tiefenauer <git@tiefenauer.info>
2016-11-07 11:02:49 +0100

    step 04

--- src/main/java/info/tiefenauer/Wjax2016Application.java
+++ src/main/java/info/tiefenauer/Wjax2016Application.java
@@ -2,8 +2,10 @@ package info.tiefenauer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class Wjax2016Application {
 
 	public static void main(String[] args) {
--- src/main/java/info/tiefenauer/model/ZwischerRepositoryImpl.java
+++ src/main/java/info/tiefenauer/model/ZwischerRepositoryImpl.java
@@ -1,5 +1,7 @@
 package info.tiefenauer.model;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -7,6 +9,7 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -24,10 +27,17 @@ public class ZwischerRepositoryImpl implements ZwischerRepository {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "noResults", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value="5000")
+    })
     public Collection<String> search(String q, int pageSize) {
         SearchResults results = twitter.searchOperations().search(q, pageSize);
         return results.getTweets().stream()
                 .map(Tweet::getUnmodifiedText)
                 .collect(toSet());
+    }
+
+    protected Collection<String> noResults(String q, int pageSize){
+        return Collections.singleton("Error getting Tweet stream. Using fallback.");
     }
 }
